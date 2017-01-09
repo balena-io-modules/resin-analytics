2dfd9fac4802cb3c0331e8d676ee84475e412d31
Eugene Mirotin <emirotin@gmail.com>
2017-01-10 01:49:14 +0300

    remove UMD, we only use these in node or webpack, both work with CJS

--- packages/resin-mixpanel-client/src/resin-mixpanel-client.js
+++ packages/resin-mixpanel-client/src/resin-mixpanel-client.js
@@ -1,92 +1,80 @@
-(function(root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		return define(['mixpanel'], function(mixpanel) {
-			return factory(mixpanel);
-		});
-	} else if (typeof exports === 'object') {
-		return module.exports = factory(require('mixpanel'));
-	}
-})(this, function(mixpanelLib) {
-	return function(token) {
+var mixpanelLib = require('mixpanel')
+module.exports = function(token) {
 
-		var mixpanel = mixpanelLib.init(token);
-		var userId = null;
+	var mixpanel = mixpanelLib.init(token)
+	var userId = null
+	var isBrowser = typeof window !== 'undefined'
 
-		mixpanel.set_config({
-			track_pageview: false
-		});
+	mixpanel.set_config({
+		track_pageview: false
+	})
 
-		var isBrowser = typeof window !== 'undefined'
-
-		return {
-			signup: function(uid, callback) {
-				if (isBrowser) {
-					mixpanel.alias(uid, uid);
-					if (typeof callback === "function") callback();
-				} else {
-					return mixpanel.alias(uid, uid, callback);
-				}
-			},
-			login: function(uid, callback) {
-				if (isBrowser) {
-					mixpanel.identify(uid);
-				}
-				userId = uid;
-				if (typeof callback === "function") callback();
-			},
-			logout: function(callback) {
-				var ref;
-				if (isBrowser) {
-					if ((ref = mixpanel.cookie) != null) {
-						ref.clear();
-					}
-				}
-				userId = null;
-				if (typeof callback === "function") callback();
-			},
-			set: function(props, callback) {
-				if (!isBrowser) {
-					if (typeof callback === "function") callback();
-				}
-				mixpanel.register(props);
-				if (typeof callback === "function") callback();
-			},
-			setOnce: function(props, callback) {
-				if (!isBrowser) {
-					if (typeof callback === "function") callback();
-				}
-				mixpanel.register_once(props);
-				if (typeof callback === "function") callback();
-			},
-			setUser: function(prop, to, callback) {
-				if (isBrowser) {
-					return mixpanel.people.set(prop, to, callback);
-				} else {
-					if (!userId) {
-						throw new Error('(Resin Mixpanel Client) Please login() before using setUser()');
-					}
-					return mixpanel.people.set(userId, prop, to, callback);
-				}
-			},
-			setUserOnce: function(prop, to, callback) {
-				if (isBrowser) {
-					return mixpanel.people.set_once(prop, to, callback);
-				} else {
-					if (!userId) {
-						throw new Error('(Resin Mixpanel Client) Please login() before using setUserOnce()');
-					}
-					return mixpanel.people.set_once(userId, prop, to, callback);
-				}
-			},
-			track: function(event, properties, callback) {
-				if (!isBrowser) {
-					properties.distinct_id = userId;
-				}
-				return mixpanel.track(event, properties, callback);
+	return {
+		signup: function(uid, callback) {
+			if (isBrowser) {
+				mixpanel.alias(uid, uid)
+				if (typeof callback === "function") callback()
+			} else {
+				return mixpanel.alias(uid, uid, callback)
 			}
-		};
+		},
+		login: function(uid, callback) {
+			if (isBrowser) {
+				mixpanel.identify(uid)
+			}
+			userId = uid
+			if (typeof callback === "function") callback()
+		},
+		logout: function(callback) {
+			var ref
+			if (isBrowser) {
+				if ((ref = mixpanel.cookie) != null) {
+					ref.clear()
+				}
+			}
+			userId = null
+			if (typeof callback === "function") callback()
+		},
+		set: function(props, callback) {
+			if (!isBrowser) {
+				if (typeof callback === "function") callback()
+			}
+			mixpanel.register(props)
+			if (typeof callback === "function") callback()
+		},
+		setOnce: function(props, callback) {
+			if (!isBrowser) {
+				if (typeof callback === "function") callback()
+			}
+			mixpanel.register_once(props)
+			if (typeof callback === "function") callback()
+		},
+		setUser: function(prop, to, callback) {
+			if (isBrowser) {
+				return mixpanel.people.set(prop, to, callback)
+			} else {
+				if (!userId) {
+					throw new Error('(Resin Mixpanel Client) Please login() before using setUser()')
+				}
+				return mixpanel.people.set(userId, prop, to, callback)
+			}
+		},
+		setUserOnce: function(prop, to, callback) {
+			if (isBrowser) {
+				return mixpanel.people.set_once(prop, to, callback)
+			} else {
+				if (!userId) {
+					throw new Error('(Resin Mixpanel Client) Please login() before using setUserOnce()')
+				}
+				return mixpanel.people.set_once(userId, prop, to, callback)
+			}
+		},
+		track: function(event, properties, callback) {
+			if (!isBrowser) {
+				properties.distinct_id = userId
+			}
+			return mixpanel.track(event, properties, callback)
+		}
+	}
 
-	};
-
-});
-
+}
