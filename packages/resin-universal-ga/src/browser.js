8e39f7c69194f3e2876f433ff1ba78172216e65d
Eugene Mirotin <emirotin@gmail.com>
2017-03-05 21:03:40 +0300

    return rejected promises from the GA methods instead of throwing

--- packages/resin-universal-ga/src/browser.js
+++ packages/resin-universal-ga/src/browser.js
@@ -4,7 +4,7 @@ var Promise = require('bluebird')
 var TRACKER_NAME = 'resinAnalytics'
 
 module.exports = function (propertyId, site, debug) {
-	loggedIn = false;
+	loggedIn = false
 
 	return {
 		login: function (userId) {
@@ -15,21 +15,21 @@ module.exports = function (propertyId, site, debug) {
 				options.cookieDomain = 'none'
 			}
 			window.ga('create', propertyId, site, TRACKER_NAME, options)
-			loggedIn = true;
+			loggedIn = true
 		},
 		logout: function () {
-			if (!loggedIn) throw new Error("GA logout called before login");
+			if (!loggedIn) return Promise.reject(new Error("GA logout called before login"))
 
 			return Promise.fromCallback(function (callback) {
 				window.ga(function() {
 					window.ga.remove(TRACKER_NAME)
-					loggedIn = false;
+					loggedIn = false
 					callback()
 				})
 			})
 		},
 		track: function (category, action, label) {
-			if (!loggedIn) throw new Error("Can't record GA events without a login first");
+			if (!loggedIn) return Promise.reject(new Error("Can't record GA events without a login first"))
 
 			return Promise.fromCallback(function (callback) {
 				var options = {
@@ -43,7 +43,7 @@ module.exports = function (propertyId, site, debug) {
 					category, action, label,
 					options
 				)
-			}).timeout(1000);
+			}).timeout(1000)
 		}
 	}
 }
